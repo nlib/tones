73315761adebcd57ab4695abd14be814c94334e1
Harpal <harpalsinghgr@gmail.com>
2015-10-30 18:27:27 +0530

    apply authorisation using env variable

--- app/controllers/tones/application_controller.rb
+++ app/controllers/tones/application_controller.rb
@@ -1,4 +1,13 @@
 module Tones
   class ApplicationController < ActionController::Base
+    before_filter :verify_user
+
+    private
+      def verify_user
+        if ENV['USER_PASSWORD'] && ENV['USER_PASSWORD'] != 'admin_user'
+          flash[:error] = 'Not authorised'
+          redirect_to "/"  
+        end
+      end
   end
 end
